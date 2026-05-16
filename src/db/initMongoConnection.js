2b4262318c131bcab36592b49d13b2142170126b
Mikosid <koyasailorziama@gmail.com>
2026-05-17 01:07:02 +0300

    ref

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,11 +9,11 @@ export const initMongoConnection = async () => {
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
     await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0import mongoose from 'mongoose'`,
+      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
-    console.log(`Error connecct database with message ${error.message}`);
+    console.log(`Error connecting to database with message ${error.message}`);
     throw error;
   }
 };
